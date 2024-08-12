ad7675b68b4a72e5c1215a706b108c943ca3111a
YehudaBenaish <135022723+YehudaBenaish@users.noreply.github.com>
2024-08-12 20:59:19 +0300

    YehudaCommit
    deletecopy and UI

--- app/src/main/java/com/example/jetsnack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetsnack/ui/theme/Theme.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.jetsnack.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -31,6 +15,7 @@ private val LightColorPalette = JetsnackColors(
     uiBackground = Neutral0,
     uiBorder = Neutral4,
     uiFloated = FunctionalGrey,
+    textPrimary = Rose7,
     textSecondary = Neutral7,
     textHelp = Neutral6,
     textInteractive = Neutral0,
@@ -39,7 +24,7 @@ private val LightColorPalette = JetsnackColors(
     iconInteractive = Neutral0,
     iconInteractiveInactive = Neutral1,
     error = FunctionalRed,
-    gradient6_1 = listOf(FunctionalRed, FunctionalRedDark, FunctionalGrey, FunctionalGreen, FunctionalDarkGrey),
+    gradient6_1 = listOf(Rose9,Rose8 ,Rose7 , Rose6, Rose3),
     gradient6_2 = listOf(Rose4, Lavender3, Rose2, Lavender3, Rose4),
     gradient3_1 = listOf(Shadow2, Ocean3, Shadow4),
     gradient3_2 = listOf(Rose2, Lavender3, Rose4),
